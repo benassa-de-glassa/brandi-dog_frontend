d8a0ed09bf008be60875935bd3a6c35cccee0dde
thilo <thiloschmid@bluewin.ch>
2020-05-10 19:45:25 +0200

    removed unnecessary import

--- src/components/topbar/UserRegistration.js
+++ src/components/topbar/UserRegistration.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import { API_URL, postData } from '../../paths'
-
 class UserRegistration extends Component {
     constructor(props) {
         super(props)
