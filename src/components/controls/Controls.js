0a191814eb750d6a3a5393e4601ab4f4b1daab91
thilo <thiloschmid@bluewin.ch>
2020-05-16 18:39:20 +0200

    added possible move descriptions

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -3,15 +3,35 @@ import React from 'react'
 import Hand from './Hand'
 import './controls.css'
 
+const possibleMoves = {
+  'A': 'Move one, eleven, or go out.',
+  '2': 'Move two.',
+  '3': 'Move three.',
+  '4': 'Move four forwards or backwards.', 
+  '5': 'Move five.',
+  '6': 'Move six.',
+  '7': 'Move seven steps. Each step can be performed individually on different marbles.',
+  '8': 'Move eight.',
+  '9': 'Move nine.',
+  '10': 'Move ten.',
+  'Ja': 'Choose an arbitrary marble to switch with one of yours. You cannot exchange locked marbles.',
+  'Q': 'Move 12.',
+  'K': 'Move 13 or go out.'
+}
+
 function Controls(props) {
 
-  var selectedCardString;
+  var selectedCardString, possibleMoveString
+
   if (props.selectedCardIndex !== null) {
     const selectedCard = props.cards[props.selectedCardIndex]
-    selectedCardString = selectedCard.color + " " + selectedCard.value
+    selectedCardString = selectedCard.color + '' + selectedCard.value
+    possibleMoveString = possibleMoves[selectedCard.value]
   } else {
-    selectedCardString = ""
+    selectedCardString = ''
+    possibleMoveString = ''
   }
+
 
   var handleClick = event => {
     event.preventDefault()
@@ -25,8 +45,8 @@ function Controls(props) {
       </div>
 
       <div className="instruction-box">
-        <p className="instruction-text">Here comes a user message such as play card</p>
         <p className="instruction-text">Currently selected card: {selectedCardString}</p>
+        <p className="instruction-text">{possibleMoveString}</p>
       </div>
       <Hand 
           cards={props.cards} 
